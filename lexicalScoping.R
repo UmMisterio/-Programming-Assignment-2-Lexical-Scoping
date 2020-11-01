3d9715c85d044ac40676eac47e6db2ad644b0716
UmMisterio <jvsdf1@gmail.com>
2020-11-01 10:09:40 -0400

    Version 1.0
    Final version.

--- lexicalScoping.R
+++ lexicalScoping.R
@@ -1,23 +1,33 @@
+## Sorry for my English
+## My function makes a matrix with the row and column you want and it's intervals
+## i set the variables and the function 'z'
+
+## This function makes a matrix with certain lines, columns and intervals that can be stored in a variable.
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
+  inverso <- NULL
+  set <- function(z) {
+    x <<- z
+    inverso <<- NULL
   }
   get <- function() x
-  setInverse <- function(inverse) inv <<- inverse
-  getInverse <- function() inv
+  setInverse <- function(inverse) inverso <<- inverse
+  getInverse <- function() inverso
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## this function get the inverse of 'x'
+## I give the inverso variable (inverso means inverse in my native language) the inverse value of 'x'
+## And i solve the matrix
+
 cachesolve <- function(x, ...) {
-  inv <- x$getInverse()
-  if(!is.null(inv)) {
+  ## Return a matrix that is the inverse of 'x'
+  inverso <- x$getInverse()
+  if(!is.null(inverso)) {
     message("Resolvendo")
-    return(inv)
+    return(inverso)
   }
   mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setInverse(inv)
-  inv
+  inverso <- solve(mat, ...)
+  x$setInverse(inverso)
+  inverso
 }
